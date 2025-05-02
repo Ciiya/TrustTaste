6e1b8a7e4534c81485de301c1fdef2f148a17666
Juan Jose Jimenez <93469601+Casper121@users.noreply.github.com>
2025-05-02 15:57:29 +0200

    feat: add icons

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import './global.css'
 
 export default function App() {
   return (
     <View className='flex-1  items-center justify-center'>
+      <Ionicons name="checkmark-circle" size={32} color="green"/>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
