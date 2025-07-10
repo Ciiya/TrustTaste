e4d5bf46fcec0137eb28199d56d163e683f47a66
Juan Jose Jimenez <93469601+Casper121@users.noreply.github.com>
2025-07-10 17:00:31 +0200

    cmaera fucking works

--- navigation/app/camera/CameraManager.tsx
+++ navigation/app/camera/CameraManager.tsx
@@ -1,92 +1,18 @@
 import React from 'react';
-import { CameraView, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import AlergenicsModal from './AlergenicsModal';
+import { StyleSheet, Text, View } from 'react-native';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from 'react-native-vision-camera';
 
 const CameraManager = () => {
-  const [scanned, setScanned] = useState(false);
-  const [permission, requestPermission] = useCameraPermissions();
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-
-  const handleBarCodeScanned = ({
-    type,
-    data,
-  }: {
-    type: string;
-    data: string;
-  }) => {
-    if (type === 'ean13' || type === 'ean8') {
-      setScanned(true);
-      setIsModalVisible(true);
-      console.log(data);
-    }
-  };
-
-  const onModalClose = () => {
-    setIsModalVisible(false);
-    setScanned(false);
-  };
-
-  if (!permission) {
-    return <View />;
-  }
-
-  if (!permission.granted) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.message}>
-          We need your permission to show the camera
-        </Text>
-        <Button onPress={requestPermission} title="Grant permission" />
-      </View>
-    );
-  }
+  const device = useCameraDevice('back');
+  //const { hasPermission } = useCameraPermission();
 
   return (
-    <View style={styles.container}>
-      <CameraView
-        style={styles.camera}
-        barcodeScannerSettings={{
-          barcodeTypes: ['ean13', 'ean8'],
-        }}
-        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
-      >
-        <AlergenicsModal isVisible={isModalVisible} onClose={onModalClose} />
-        <View style={styles.buttonContainer} />
-      </CameraView>
-    </View>
+    <Camera style={StyleSheet.absoluteFill} device={device!} isActive={true} />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  message: {
-    textAlign: 'center',
-    paddingBottom: 10,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
 
 export default CameraManager;
